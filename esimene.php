b9de95f62f892a863ec8fb9e3c04cfe31ab8d88b
Markus Heinsalu <heinsalum@users.noreply.github.com>
2017-09-11 10:37:01 +0300

    HTML koodile lisatud esimene .php osa

---
+++ esimene.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Markus Heinsalu veebiprogemise asjad</title>
+</head>
+<body>
+	<h1>Markus Heinsalu</h1>
+	<p>See veebileht on loodud veebiprogrammeerimise kursusel ning ei sisalda mingisugust tõsiseltvõetavat sisu.</p>
+	
+
+	<p> Mina olen 19-aastane linnavurle Tallinnast, tegelen korvpalliga ja olen 2 meetrit pikk. Mulle väga meeldib Tallinna Ülikool, sealhulgas ained, õppejõud kui ka kaastudengid.</p>
+	
+	<?php
+		echo "<p>Täna on vastik ilm</p>";
+	?>
+	
+</body>
+</html>
