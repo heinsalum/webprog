754f9e2468785853e82287560f84156e325aa402
Markus Heinsalu <heinsalum@users.noreply.github.com>
2017-10-30 11:36:28 +0200

    tunnikäkerdis

---
+++ tund8/main.php
@@ -0,0 +1,60 @@
+<?php
+	require("functions.php");
+	
+	//kui pole sisse loginud, siis sisselogimise lehele
+	if (!isset($_SESSION["userId"])){
+		header("Location: login.php");
+		exit();
+	}	
+	
+	//kui logid välja:
+	if (isset($_GET["logout"])){
+		//lõpetame sessiooni
+		session_destroy();
+		header("Location: login.php");
+	}
+
+	$dirToRead = "../../Pics/";
+	// kuna tahan ainult pildifaile, siis filtreerin
+	$picFileTypes = ["jpg", "jpeg", "png", "gif"];
+	$picFiles = [];
+	// $allFiles = scandir($dirToRead);
+	// loen kataloogi ja viskan kaks esimest massiivi liiget(. ja ..) välja
+	$allFiles = array_slice(scandir($dirToRead),2);
+	// var_dump($allFiles);
+	
+	// tsükkel, mis töötab ainult massiividega
+	foreach ($allFiles as $file){
+		$fileType = pathinfo($file, PATHINFO_EXTENSION);
+		// Kas see tüüp on lubatud nimekirjas?
+		if (in_array($fileType, $picFileTypes) == true){
+			array_push($picFiles, $file);
+		}
+	} //foreach lõpp
+	//var_dump($picFiles);
+	
+	// Mitu pilti on?
+	$fileCount = count($picFiles);
+	$picNumber = mt_rand(0, $fileCount - 1);
+	$picToShow = $picFiles[$picNumber];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Markus Heinsalu veebiprogemise asjad</title>
+</head>
+<body>
+	<h1>Markus Heinsalu</h1>
+	<p>See veebileht on loodud veebiprogrammeerimise kursusel ning ei sisalda mingisugust tõsiseltvõetavat sisu.</p>
+	<p><a href="?logout=1">Logi välja!</a></p>
+	<p><a href="usersinfo.php">Kasutajate info</a></p>
+	<p><a href="usersideas.php">Kasutajate ideed</a></p>
+	<p>Üks pilt Tallinna Ülikoolist!</p>
+	<img src="<?php echo $dirToRead .$picToShow; ?>" alt="Tallinna Ülikool">
+	
+	<h1>Sisse on loginud kasutaja:</h1>
+	
+</body>
+</html>
---
+++ tund8/usersinfo.php
@@ -0,0 +1,65 @@
+<?php
+	require("functions.php");
+	
+	//kui pole sisse loginud, siis sisselogimise lehele
+	if (!isset($_SESSION["userId"])){
+		header("Location: login.php");
+		exit();
+	}	
+	
+	//kui logid välja:
+	if (isset($_GET["logout"])){
+		//lõpetame sessiooni
+		session_destroy();
+		header("Location: login.php");
+	}
+	
+	/*
+	while($stmt->fetch()){
+		
+	}
+	*/
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Markus Heinsalu veebiprogemise asjad</title>
+</head>
+<body>
+	<h1>Markus Heinsalu</h1>
+	<p>See veebileht on loodud veebiprogrammeerimise kursusel ning ei sisalda mingisugust tõsiseltvõetavat sisu.</p>
+	<p><a href="?logout=1">Logi välja!</a></p>
+	<p><a href="main.php">Pealeht!</a></p>
+	<hr>
+	<h2>Kõik systeemi kasutajad</h2>
+	<table "border:"1" style="border: 1px solid black; border-collapse: collapse">
+	<tr>
+		<th>Eesnimi</th><th>Perekonnanimi</th><th>E-posti aadress</th>
+	</tr>
+	<tr>
+		<td>Juku</td><td>Porgand</td><td>juku@gmail.com</td>
+	</tr>
+		<td>John</td><td>Konn</td><td>john@gmail.com</td>
+	</tr>
+		<td>Peeter</td><td>Pann</td><td>peeter@gmail.com</td>
+	</tr>
+		<td>Timo</td><td>Till</td><td>timo@gmail.com</td>
+	</tr>
+		<td>Tauri</td><td>Tuisk</td><td>tauri@gmail.com</td>
+	</tr>
+		<td>Tarvi</td><td>Tuul</td><td>tarvi@gmail.com</td>
+	</tr>
+		<td>Tarvo</td><td>Tuuker</td><td>tarvo@gmail.com</td>
+	</tr>
+		<td>Karl</td><td>Suur</td><td>karl@gmail.com</td>
+	</tr>
+		<td>Mark</td><td>Murakas</td><td>mark@gmail.com</td>
+	</tr>
+		<td>Mehis</td><td>Maasikas</td><td>mehis@gmail.com</td>
+	</tr>
+	</table>
+	
+</body>
+</html>
